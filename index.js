b2e258348061994450cd0eafd990c5a5c2192cc9
Elevate <124959588+i7ector@users.noreply.github.com>
2023-02-27 22:46:52 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function alertUserR() {
+    alert('Hey, you clicked the Register button!')
+}
+
+function alertUserF() {
+    alert('Hey, you clicked the Facebook button!')
+}
+
+function alertUserT() {
+    alert('Hey, you clicked the Twitter button!')
+}
+
+function alertUserI() {
+    alert('Hey, you clicked the Instagram button!')
+}
